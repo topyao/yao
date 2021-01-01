f1bca6f5297fedf397b58f5a533e52ab86c3a76d
chengyao <bigyao@139.com>
2021-01-01 08:33:22 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ROOT', dirname(getcwd()) . '/');
-require ROOT . 'vendor/autoload.php';
+define('ROOT', dirname(getcwd()) . DIRECTORY_SEPARATOR);
+require ROOT . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 //调用框架初始化方法
 Yao\App::run();
