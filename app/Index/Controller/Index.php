00c29a0250628463fdb94819d7cc3d2cdeed3261
root <root@DESKTOP-1SDORV4.localdomain>
2021-01-06 21:03:08 +0800

    项目部署

--- app/Index/Controller/Index.php
+++ app/Index/Controller/Index.php
@@ -2,10 +2,32 @@
 
 namespace App\Index\Controller;
 
+use Yao\Db;
+use Yao\Facade\File;
+
 class Index
 {
     public function index()
     {
-        abort('可以开始了!', 200);
+        $file = Db::name('files')->field(['file', 'filename', 'md5'])->order(['id' => 'desc'])->select()->toArray();
+        foreach ($file as $k => $v) {
+            $filesize = file_exists($file[$k]['file']) ? format_size(filesize($file[$k]['file'])) : 'Na';
+            $file[$k]['size'] = $filesize;
+            unset($file[$k]['file']);
+        }
+        return $file;
+    }
+
+    public function todo()
+    {
+        return view('index@todo');
+    }
+
+    public function download(\Yao\Http\Request $request)
+    {
+        $file = Db::name('files')->where(['md5' => $request->get('hash')])->find();
+        if (!empty($file)) {
+            return File::download($file['filename'], $file['file']);
+        }
     }
 }
