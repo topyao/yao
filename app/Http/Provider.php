7da0aa99d7585a38c2b9c5dbbee3cd098e646167
chengyao <chengyao1@foxmail.com>
2021-01-12 12:20:46 +0800

    optimized

--- app/Http/Provider.php
+++ app/Http/Provider.php
@@ -3,13 +3,13 @@
 
 namespace App\Http;
 
-class Provider extends \Yao\Provider
+class Provider extends \Yao\Provider\Provider
 {
     public function services()
     {
         return [
             //线上环境可以删除
-            \Yao\Services\Development::class
+            \Yao\Provider\Services\Development::class
         ];
     }
 }
