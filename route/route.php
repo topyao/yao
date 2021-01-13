18b89e401144595da9a6689fa1bb695f6b99cd75
chengyao <bigyao@139.com>
2021-01-13 21:07:16 +0800

    update

--- route/route.php
+++ route/route.php
@@ -6,10 +6,4 @@
 
 use \Yao\Facade\Route;
 
-Route::get('/', [\App\Index\Controller\Index::class, 'index'])->alias('name')->cross('*');
-
-Route::post('/upload', 'index@index/upload');
-
-Route::rule('test', 'index@index/test');
-
-Route::get('todo', 'index@index/todo');
+Route::get('/', [\App\Index\Controller\Index::class, 'index']);
