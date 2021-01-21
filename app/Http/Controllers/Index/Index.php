a7385327dd2ba36a4f65a15c6338bbb7593caa91
chengyao <bigyao@139.com>
2021-01-21 20:00:00 +0800

    开发版本

--- app/Http/Controllers/Index/Index.php
+++ app/Http/Controllers/Index/Index.php
@@ -11,47 +11,6 @@ class Index
 
     public function index(Request $request)
     {
-        return view('index/index');
-        $file = Db::name('files')
-            ->field(['file', 'filename', 'md5'])
-            ->order(['id' => 'desc'])
-            ->select()
-            ->toArray();
-        foreach ($file as $k => $v) {
-            $filesize = file_exists($file[$k]['file'])
-                ? format_size(filesize($file[$k]['file']))
-                : 'Na';
-            $file[$k]['size'] = $filesize;
-            unset($file[$k]['file']);
-        }
-
-        return $file;
-    }
-
-
-    public function upload()
-    {
-    }
-
-    public function todo()
-    {
-        return view('index/todo');
-    }
-
-    public function download(Request $request)
-    {
-        $file = Db::name('files')->where(['md5' => $request->get('hash')])->find();
-        return File::download($file['filename'], $file['file']);
-    }
-
-    public function document()
-    {
-        return view('index/document');
-    }
-
-
-    public function login()
-    {
-        return view('index/login');
+        abort('可以开始了！', 200);
     }
 }
