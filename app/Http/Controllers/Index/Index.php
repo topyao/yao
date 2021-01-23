010ba96c070a42e569b557ac56268dcecdda1f7f
chengyao <bigyao@139.com>
2021-01-23 16:53:38 +0800

    composer配置文件添加了require的配置，添加了跨域的配置

--- app/Http/Controllers/Index/Index.php
+++ app/Http/Controllers/Index/Index.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Controllers\Index;
 
-use App\Models\Notes;
-use Yao\{Http\Request};
-use Yao\Facade\Db;
+use Yao\Http\Request;
 
 class Index
 {
-
     public function index(Request $request)
     {
         abort('可以开始了！', 200);
