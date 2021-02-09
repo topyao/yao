52907f01be23563446846e4d321d78b848e50e12
chengyao <bigyao@139.com>
2021-02-09 22:35:46 +0800

    添加了基础控制器的一些方法

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -2,6 +2,18 @@
 
 namespace App\Http;
 
+use Yao\App;
+
 class Controller extends \Yao\Controller
 {
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+    }
+
+    public function init()
+    {
+        parent::init();
+    }
 }
