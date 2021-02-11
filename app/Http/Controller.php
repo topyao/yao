6a9ebaf75d782666997b05e781ddbfe71298843d
chengyao <bigyao@139.com>
2021-02-11 11:47:07 +0800

    基础控制器的构造方法不再可以被重写

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -2,18 +2,11 @@
 
 namespace App\Http;
 
-use Yao\App;
-
 class Controller extends \Yao\Controller
 {
 
-    public function __construct(App $app)
-    {
-        parent::__construct($app);
-    }
-
     public function init()
     {
-        parent::init();
+
     }
 }
