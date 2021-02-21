287380f3bf3b85dd052e66c821d3923fe8d415b5
chengyao <bigyao@139.com>
2021-02-21 12:45:50 +0800

    update

--- app/Http/Controllers/Index/Index.php
+++ app/Http/Controllers/Index/Index.php
@@ -2,23 +2,20 @@
 
 namespace App\Http\Controllers\Index;
 
-use App\Http\{Controller, Traits\Paginate};
+use App\Http\{Controller};
 use App\Models\Notes;
 use Yao\Facade\Cache;
 
 class Index extends Controller
 {
-
-    use Paginate;
-
     public function index(Notes $notes)
     {
         try {
             $stat = (int)Cache::get('stat');
+            Cache::set('stat', ++$stat);
         } catch (\Exception $e) {
             $stat = 0;
         }
-        Cache::set('stat', ++$stat);
         return view('index/index', compact(['stat']));
     }
 }
