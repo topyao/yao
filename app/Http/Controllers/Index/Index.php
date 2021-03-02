e4e5a2f9a3cfed457b030c7faf022e12da1252e5
chengyao <bigyao@139.com>
2021-03-02 20:19:34 +0800

    更新了欢迎界面

--- app/Http/Controllers/Index/Index.php
+++ app/Http/Controllers/Index/Index.php
@@ -9,7 +9,17 @@ class Index extends Controller
 {
     public function index(Request $request)
     {
-        abort('可以开始了！', 200);
+        echo <<<EOT
+<title>Welcome !</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div style="height: 60vh;border:1px solid #d4d4d4;margin:19vh 10vw">
+    <div style="background-color: #1E90FF;line-height:3em;padding:0 1em;height: 3em;color: white;font-weight: bold">Welcome!</div>
+<div style="text-align: center;display: flex;justify-content: center;"><p>我比较懒，这里还没有写</p></div>
+
+</div>
+EOT;
+
+//        abort('可以开始了！', 200);
     }
 
     public function test()
