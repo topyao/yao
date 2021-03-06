73ef6e22058d881c1a64799cf454e7c9662ceb2f
chengyao <bigyao@139.com>
2021-03-06 10:25:51 +0800

    update

--- app/Http/Controllers/Index/Index.php
+++ app/Http/Controllers/Index/Index.php
@@ -15,11 +15,8 @@ class Index extends Controller
 <div style="height: 60vh;border:1px solid #d4d4d4;margin:19vh 10vw">
     <div style="background-color: #1E90FF;line-height:3em;padding:0 1em;height: 3em;color: white;font-weight: bold">Welcome!</div>
 <div style="text-align: center;display: flex;justify-content: center;"><p>我比较懒，这里还没有写</p></div>
-
 </div>
 EOT;
-
-//        abort('可以开始了！', 200);
     }
 
     public function test()
