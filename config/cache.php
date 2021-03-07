2c27b9697985cd6550a796daaf2a90d80cd8a17d
chengyao <bigyao@139.com>
2021-03-07 20:51:27 +0800

    update

--- config/cache.php
+++ config/cache.php
@@ -2,12 +2,13 @@
 
 return [
     //缓存类型
-    'default' => 'file',
+    'default' => 'redis',
     // redis缓存
     'redis' => [
         'host' => 'localhost',
         'port' => 6379,
         'auth' => 'cheng',
+        'timeout' => 10
     ],
     //文件缓存
     'file' => [
